b7fd27f336d2604f1c1f68eb4cc7f285b3b5c308
Jrt345 <JonTech543@protonmail.com>
2021-08-01 11:08:31 -0700

    Create basic aboutBox.fxml layout.
    Add bookJarTest2-200x.png to be used in aboutBox.fxml.

--- src/main/controllers/AboutBoxController.java
+++ src/main/controllers/AboutBoxController.java
@@ -1,4 +1,12 @@
 package main.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class AboutBoxController {
+
+    @FXML
+    public void closeAboutBox(ActionEvent event) {
+
+    }
 }
