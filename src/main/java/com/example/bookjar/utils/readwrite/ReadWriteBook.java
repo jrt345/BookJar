7211560b6d42978b0c5f3dd2808404f384373353
Jrt345 <JonTech543@protonmail.com>
2022-06-24 10:37:55 -0700

    Implement equals, hashCode, and toString methods in ReadWriteBook.java

--- src/main/java/com/example/bookjar/utils/readwrite/ReadWriteBook.java
+++ src/main/java/com/example/bookjar/utils/readwrite/ReadWriteBook.java
@@ -1,7 +1,8 @@
-package com.example.bookjar.utils;
+package com.example.bookjar.utils.readwrite;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Objects;
 
 public class ReadWriteBook implements Serializable {
 
@@ -64,5 +65,40 @@ public class ReadWriteBook implements Serializable {
 
     public void setNotes(String notes) {
         this.notes = notes;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ReadWriteBook that = (ReadWriteBook) o;
+
+        if (index != that.index) return false;
+        if (!Objects.equals(title, that.title)) return false;
+        if (!Objects.equals(author, that.author)) return false;
+        if (!Objects.equals(genre, that.genre)) return false;
+        return Objects.equals(notes, that.notes);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = index;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (author != null ? author.hashCode() : 0);
+        result = 31 * result + (genre != null ? genre.hashCode() : 0);
+        result = 31 * result + (notes != null ? notes.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "ReadWriteBook{" +
+                "index=" + index +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", genre='" + genre + '\'' +
+                ", notes='" + notes + '\'' +
+                '}';
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,4 +8,6 @@ module com.example.bookJar {
     exports com.example.bookjar.controllers;
     opens com.example.bookjar.utils to javafx.fxml;
     exports com.example.bookjar.utils;
+    exports com.example.bookjar.utils.readwrite;
+    opens com.example.bookjar.utils.readwrite to javafx.fxml;
 }
