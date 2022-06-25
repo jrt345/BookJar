022d0f988bfbfdf9f98bcfb72a6d6da1e3b5141a
Jrt345 <JonTech543@protonmail.com>
2022-06-24 17:02:24 -0700

    Create Tag.java class

---
+++ src/main/java/com/example/bookjar/library/Tag.java
@@ -0,0 +1,36 @@
+package com.example.bookjar.library;
+
+public class Tag {
+
+    private String string;
+
+    public String getString() {
+        return string;
+    }
+
+    public void setString(String string) {
+        this.string = string;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Tag tag = (Tag) o;
+
+        return string.equals(tag.string);
+    }
+
+    @Override
+    public int hashCode() {
+        return string.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "string='" + string + '\'' +
+                '}';
+    }
+}
