1118ec3c3f9d0e0915566c658d5bd6827e9dfcb3
Jrt345 <JonTech543@protonmail.com>
2022-06-28 20:30:17 -0700

    Rename LoadBookJar.java to App.java and refactor in Main.java

--- src/main/java/com/example/bookjar/App.java
+++ src/main/java/com/example/bookjar/App.java
@@ -9,11 +9,11 @@ import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
 
-public class LoadBookJar extends Application {
+public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LoadBookJar.class.getResource("bookJar.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("bookJar.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
         stage.setTitle("BookJar");
         stage.setScene(scene);
--- src/main/java/com/example/bookjar/Main.java
+++ src/main/java/com/example/bookjar/Main.java
@@ -2,6 +2,6 @@ package com.example.bookjar;
 
 public class Main {
     public static void main(String[] args){
-        LoadBookJar.main(args);
+        App.main(args);
     }
 }
