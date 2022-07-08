c31f51fe166042436af2a3b7ac9cebe75fb903b1
Jrt345 <JonTech543@protonmail.com>
2022-07-07 18:27:17 -0700

    Create stylesheet.css for bookJar.fxml UI and add icons for new UI

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -14,7 +14,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("bookJar.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 900, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 950, 600);
         stage.setTitle("BookJar");
         stage.setScene(scene);
         stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("images/bookJarLogo-200x.png"))));
