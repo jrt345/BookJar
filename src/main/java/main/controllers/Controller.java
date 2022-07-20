c93e32caaba6948e55b1edebf74c07d9c1a32788
Jrt345 <JonTech543@protonmail.com>
2022-07-20 16:33:27 -0700

    Add onAction events to buttons in bookJar.fxml and Controller.java

--- src/main/java/main/controllers/Controller.java
+++ src/main/java/main/controllers/Controller.java
@@ -1,11 +1,38 @@
 package main.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
+
+    @FXML
+    private void about(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void github(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void library(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void settings(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void update(ActionEvent event) {
+
+    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
